5644a6e31bcd638784e2f9ba2cb55525a30d540c
rudra-404 <rudrakaiser840@gmail.com>
2025-04-09 00:20:15 +0600

    C++

---
+++ SOLVE/Repetitions.cpp
@@ -0,0 +1,38 @@
+/* A u t h o r :
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╱╭╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╱╱┃┃┃╭╯
+┃╰━╯┣╮╭┳━╯┣━┳━━╮┃╰╯╯╭━━┳┳━━┳━━┳━╮
+┃╭╮╭┫┃┃┃╭╮┃╭┫╭╮┃┃╭╮┃┃╭╮┣┫━━┫┃━┫╭╯
+┃┃┃╰┫╰╯┃╰╯┃┃┃╭╮┃┃┃┃╰┫╭╮┃┣━━┃┃━┫┃
+╰╯╰━┻━━┻━━┻╯╰╯╰╯╰╯╰━┻╯╰┻┻━━┻━━┻╯
+
+R u d r a    K a i s e r */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define ll long long
+#define endl '\n'
+
+void solve() {
+    string s;
+    cin >> s;
+    int count = 1, maximum = 1;
+    for (int i = 1; i < s.size(); i++) {
+        if (s[i - 1] == s[i]) {
+            count++;
+        } else {
+            maximum = max(maximum, count);
+            count = 1;
+        }
+    }
+    maximum = max(maximum, count);
+    cout << maximum << endl;
+}
+
+int main() {
+    fast;
+    solve();
+    return 0;
+}
